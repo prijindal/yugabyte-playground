5432c7f216f372af37b84d3a54ddd7ae5ee1f1a3
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2025-08-19 15:34:25 +0000

    Moved to ip address based hostnames in single zone deployment

--- singlezone/create_user.sh
+++ singlezone/create_user.sh
@@ -1,2 +1,2 @@
-podman exec -it ybtserver0 ysqlsh "postgres://yugabyte:yugabyte@ybtserver0,ybtserver1,ybtserver2:5433/yugabyte" -c "CREATE USER workload WITH PASSWORD 'workload';" &&
-podman exec -it ybtserver0 ysqlsh "postgres://yugabyte:yugabyte@ybtserver0,ybtserver1,ybtserver2:5433/yugabyte" -c "CREATE DATABASE workload OWNER workload;"
+podman exec -it ybtserver0 ysqlsh "postgres://yugabyte:yugabyte@10.42.1.200,10.42.1.201,10.42.1.202:5433/yugabyte" -c "CREATE USER workload WITH PASSWORD 'workload';" &&
+podman exec -it ybtserver0 ysqlsh "postgres://yugabyte:yugabyte@10.42.1.200,10.42.1.201,10.42.1.202:5433/yugabyte" -c "CREATE DATABASE workload OWNER workload;"
--- singlezone/enable_rest_encryption.sh
+++ singlezone/enable_rest_encryption.sh
@@ -1,5 +1,5 @@
 podman exec -it ybmaster0 openssl rand -out /tmp/universe_key 48 &&
-podman exec -it ybmaster0 yb-admin --master_addresses ybmaster0,ybmaster1,ybmaster2 add_universe_key_to_all_masters master_key /tmp/universe_key &&
-podman exec -it ybmaster0 yb-admin --master_addresses ybmaster0,ybmaster1,ybmaster2 all_masters_have_universe_key_in_memory master_key &&
-podman exec -it ybmaster0 yb-admin --master_addresses ybmaster0,ybmaster1,ybmaster2 rotate_universe_key_in_memory master_key
+podman exec -it ybmaster0 yb-admin --master_addresses 10.42.1.100,10.42.1.101,10.42.1.102 add_universe_key_to_all_masters master_key /tmp/universe_key &&
+podman exec -it ybmaster0 yb-admin --master_addresses 10.42.1.100,10.42.1.101,10.42.1.102 all_masters_have_universe_key_in_memory master_key &&
+podman exec -it ybmaster0 yb-admin --master_addresses 10.42.1.100,10.42.1.101,10.42.1.102 rotate_universe_key_in_memory master_key
 
