5468d7f609f23548d0187ca1531a4b60f73f282d
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2025-08-20 05:40:03 +0000

    Improved scripts for single zone

--- singlezone/create_user.sh
+++ singlezone/create_user.sh
@@ -1,2 +1,5 @@
-podman exec -it ybtserver0 ysqlsh "postgres://yugabyte:yugabyte@10.42.1.200,10.42.1.201,10.42.1.202:5433/yugabyte" -c "CREATE USER workload WITH PASSWORD 'workload';" &&
-podman exec -it ybtserver0 ysqlsh "postgres://yugabyte:yugabyte@10.42.1.200,10.42.1.201,10.42.1.202:5433/yugabyte" -c "CREATE DATABASE workload OWNER workload;"
+YB_TSERVERS="10.42.1.200,10.42.1.201,10.42.1.202"
+
+podman exec -it ybtserver0 ysqlsh "postgres://yugabyte:yugabyte@$YB_TSERVERS:5433/yugabyte" -c "ALTER ROLE yugabyte WITH PASSWORD 'yugabyte';" &&
+podman exec -it ybtserver0 ysqlsh "postgres://yugabyte:yugabyte@$YB_TSERVERS:5433/yugabyte" -c "CREATE USER workload WITH PASSWORD 'workload';" &&
+podman exec -it ybtserver0 ysqlsh "postgres://yugabyte:yugabyte@$YB_TSERVERS:5433/yugabyte" -c "CREATE DATABASE workload OWNER workload;"
--- singlezone/enable_rest_encryption.sh
+++ singlezone/enable_rest_encryption.sh
@@ -1,5 +1,12 @@
-podman exec -it ybmaster0 openssl rand -out /tmp/universe_key 48 &&
-podman exec -it ybmaster0 yb-admin --master_addresses 10.42.1.100,10.42.1.101,10.42.1.102 add_universe_key_to_all_masters master_key /tmp/universe_key &&
-podman exec -it ybmaster0 yb-admin --master_addresses 10.42.1.100,10.42.1.101,10.42.1.102 all_masters_have_universe_key_in_memory master_key &&
-podman exec -it ybmaster0 yb-admin --master_addresses 10.42.1.100,10.42.1.101,10.42.1.102 rotate_universe_key_in_memory master_key
+YB_MASTERS="10.42.1.100,10.42.1.101,10.42.1.102"
 
+IS_ENABLED=$(podman exec -it ybmaster0 yb-admin --master_addresses $YB_MASTERS is_encryption_enabled | grep ENABLED)
+
+if [ -z "$IS_ENABLED" ]; then
+    podman exec -it ybmaster0 openssl rand -out /tmp/universe_key 48 &&
+    podman exec -it ybmaster0 yb-admin --master_addresses $YB_MASTERS add_universe_key_to_all_masters master_key /tmp/universe_key &&
+    podman exec -it ybmaster0 yb-admin --master_addresses $YB_MASTERS all_masters_have_universe_key_in_memory master_key &&
+    podman exec -it ybmaster0 yb-admin --master_addresses $YB_MASTERS rotate_universe_key_in_memory master_key
+else
+    echo "Encryption is already enabled"
+fi
